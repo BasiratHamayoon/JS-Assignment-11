8eec5564423cc7eb86b396c322050a813db527d8
Basirat Hamayoon <basirathumayun@gmail.com>
2024-07-07 18:18:19 +0530

    Problem no 1 solved

---
+++ script.js
@@ -0,0 +1,15 @@
+//Problem 01
+
+// var mainArray = []
+// for (var i = 0; i < 5; i++) {
+//     var userInput = +prompt("Please enter elements to the Array");
+//     mainArray.push(userInput);
+//     console.log(mainArray[i]);
+// }
+// let smallerNum = mainArray[0];
+// for (let i = 0; i < mainArray.length; i++) {
+//     if (mainArray[i] < smallerNum) {
+//         smallerNum = mainArray[i];
+//     }
+// }
+// console.log(`The Smallest Number in this Array is: ${smallerNum}`)
